8eb3aa065f90bde0895245ec60556f19cdf0b2f1
TheNetAdmin <wzxlovesy@gmail.com>
2018-03-28 12:48:27 +0800

    Fix missing boot.s
    - Fix missing boot.s
    - Add format tool
    - Format source code

--- src/io.c
+++ src/io.c
@@ -1,15 +1,15 @@
 
-#include "inttypes.h"
 #include "io.h"
+#include "inttypes.h"
 
 uint8_t iportb(uint32_t port)
 {
-    uint8_t *addr = (uint8_t *)(0xb4000000 | port);
-    return (uint8_t)(*addr);
+	uint8_t *addr = (uint8_t *)(0xb4000000 | port);
+	return (uint8_t)(*addr);
 }
 
 void oportb(uint32_t port, uint8_t data)
 {
-    uint8_t *addr = (uint8_t *)(0xb4000000 | port);
-    *addr = data;
+	uint8_t *addr = (uint8_t *)(0xb4000000 | port);
+	*addr = data;
 }

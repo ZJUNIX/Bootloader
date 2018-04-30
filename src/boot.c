d07c4d3a1a75d2e5458a3f7817a03d2e3256bca6
TheNetAdmin <zxwang42@gmail.com>
2018-04-30 17:57:49 +0800

    Clean files and code

--- src/boot.c
+++ src/boot.c
@@ -3,19 +3,19 @@
 
 int test_global = 0;
 
-void boot(){
-    vga_init();
-    int data[512];
-    data[10] = 0;
-    if(test_global!=0)
-    {
-        vga_test();
-    }
-    //
-    //put_string("1111", 5, 1, 1);
-    //put_string("2222", 5, 2, 2);
-    int test_num_int = 2018;
-    char c = 'a';
-    kernel_printf("Hello %d %c\n 888", test_num_int, 'a');
-     //put_string("2222", 20, 2, 2);
+void boot()
+{
+	vga_init();
+	int data[512];
+	data[10] = 0;
+	if (test_global != 0) {
+		vga_test();
+	}
+	//
+	// put_string("1111", 5, 1, 1);
+	// put_string("2222", 5, 2, 2);
+	int test_num_int = 2018;
+	char c = 'a';
+	kernel_printf("Hello %d %c\n 888", test_num_int, 'a');
+	// put_string("2222", 20, 2, 2);
 }

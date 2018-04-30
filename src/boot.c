31ece9a1c89421d4c3664b520ff2fa722d7050b1
Weixin-Liang <liangweixin97@outlook.com>
2018-04-30 19:10:43 +0900

    test and fail

--- src/boot.c
+++ src/boot.c
@@ -1,14 +1,19 @@
 #include "vga.h"
 #include "vga_kernel_print.h"
 
-int test = 0;
+int test_global = 0;
 
 void boot(){
     vga_init();
     int data[512];
     data[10] = 0;
-
-    if(test==0)
+    if(test_global==0)
+    {
         vga_test();
+    }
+    //
+    //put_string("1111", 5, 1, 1);
+    //put_string("2222", 5, 2, 2);
     //kernel_printf("Hello");
+     //put_string("2222", 20, 2, 2);
 }

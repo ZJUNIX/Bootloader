52633eeb05e650c02f226e7f2e31453b6ff9845c
TheNetAdmin <wzxlovesy@gmail.com>
2018-05-11 22:15:38 +0800

    Run kernel

--- src/boot.c
+++ src/boot.c
@@ -1,14 +1,20 @@
 #include "ata.h"
 #include "kernel_loader.h"
 #include "vga.h"
+
+void goto_kernel()
+{
+	void (*kernel_entry)() = (void (*)())0x80001000;
+	kernel_entry();
+}
+
 void boot()
 {
 	vga_init();
-	int data[512];
-	data[10] = 0;
 	vga_test();
 	ata_init();
 	load_kernel();
+	goto_kernel();
 	while (1)
 		;
 }
